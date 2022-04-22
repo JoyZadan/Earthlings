7988f3db7baae6b24bf2797f23d323eab94dde14
Alex <pioneerko@hotmail.com>
2022-04-22 16:24:57 +0100

    Added register page username entry validation

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,32 @@
 /*jshint esversion: 6 */
 console.log('hello im working')
+
+
+
+function isUserNameValid(username) {
+  /*
+    Usernames can only have:
+    - Lowercase Letters (a-z)
+    - Numbers (0-9)
+    - Underscores (_)
+  */
+  const res = /^[a-z0-9_]+$/.exec(username);
+  const valid = !!res;
+  return valid;
+}
+
+
+//Password matching validation
+
+function validateInput(id) {
+    let element = document.getElementById(id);
+    if (id === 'username') {
+        const username = element.value
+        if (isUserNameValid(username)) {
+            console.log('username valid')
+            document.getElementById(`${id}_error`).innerHTML='';
+        }else {
+             document.getElementById(`${id}_error`).innerHTML='Username can only contain a-z, 0-9 or _';
+        }
+    }
+}
