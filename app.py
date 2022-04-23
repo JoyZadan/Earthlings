61061f8733890b98642fe0baf6f63932ed18c697
Alex <pioneerko@hotmail.com>
2022-04-23 16:37:56 +0100

    blog page functionality to display blogs records

--- app.py
+++ app.py
@@ -177,8 +177,8 @@ def add_category():
 
 @app.route('/blog')
 def blog():
-    blog_list = mongo.db.blog.find()
-    category_list = mongo.db.categories.find()
+    blog_list = list(mongo.db.blog.find())
+    category_list = list(mongo.db.categories.find())
 
     return render_template('blog.html', blog_list=blog_list, category_list=category_list)
 
